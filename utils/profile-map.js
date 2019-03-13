d14f968dee6dbb2fe61b474e6281706eb42d7344
Andrew Wanex <awanex2705@gmail.com>
2019-03-13 11:18:35 -0700

    feat: add profile value mapping function

---
+++ utils/profile-map.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = (user, body) => {
+  if (body.username) {
+    user.username = body.username;
+  }
+  if (body.firstname) {
+    user.firstname = body.firstname;
+  }
+  if (body.lastname) {
+    user.lastname = body.lastname;
+  }
+  if (body.language) {
+    user.language = body.language;
+  }
+  if (body.country) {
+    user.country = body.country;
+  }
+  if (body.preferredSources) {
+    user.preferredSources = body.preferredSources;
+  }
+};
