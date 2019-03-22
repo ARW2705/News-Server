68ef4d9a58c3c55f7bd34e51049e7d312a1bbb63
Andrew Wanex <awanex2705@gmail.com>
2019-03-22 12:07:45 -0700

    refactor: remove cors calls - now handled by proxy server

--- routes/newsRouter.js
+++ routes/newsRouter.js
@@ -3,18 +3,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const authenticate = require('authenticate');
+const authenticate = require('../authenticate');
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
 const User = require('../models/user');
-const cors = require('cors');
 
 const newsRouter = express.Router();
 
 newsRouter.use(bodyParser.json());
 
+// Get headlines - user login NOT required
 newsRouter.route('/')
-  .get(cors(), (req, res, next) => {
+  .get((req, res, next) => {
     const options = {
       country: 'us',
       category: 'general',
@@ -26,40 +26,17 @@ newsRouter.route('/')
       }
     }
     newsapi.v2.topHeadlines(options)
-    .then(response => {
-      res.statusCode = 200;
-      res.setHeader('content-type', 'application/json');
-      res.json(response);
-    })
-    .catch(err => next(err));
-  });
-
-newsRouter.route('/top/:id')
-  .get(authenticate.verifyUser, (req, res, next) => {
-    User.findById(req.user.id)
-      .then(user => {
-        const options = {
-          country: 'us',
-          category: 'general',
-          page: 1
-        };
-        if (user.id == req.params.id
-            && user.preferredSources.length) {
-          options['sources'] = user.preferredSources;
-        }
-        newsapi.v2.topHeadlines(options)
-          .then(response => {
-            res.statusCode = 200;
-            res.setHeader('content-type', 'application/json');
-            res.json(response);
-          })
-          .catch(err => next(err));
+      .then(response => {
+        res.statusCode = 200;
+        res.setHeader('content-type', 'application/json');
+        res.send(response);
       })
       .catch(err => next(err));
   });
 
+// Perform query - user login NOT required
 newsRouter.route('/search')
-  .get(cors(), (req, res, next) => {
+  .get((req, res, next) => {
     const options = {
       language: 'en',
       country: 'us',
@@ -77,12 +54,55 @@ newsRouter.route('/search')
       .then(response => {
         res.statusCode = 200;
         res.setHeader('content-type', 'application/json');
-        res.json(response);
+        res.send(response);
       })
       .catch(err => next(err));
   });
 
-newsRouter.route('/search/:id')
+// Get a list of available api sources - user login NOT required
+newsRouter.route('/sources')
+  .get((req, res, next) => {
+    const options = {};
+    if (req.query) {
+      for (const key in req.query) {
+        options[key] = req.query[key];
+      }
+    }
+    newsapi.v2.sources(options)
+      .then(response => {
+        res.statusCode = 200;
+        res.setHeader('content-type', 'application/json');
+        res.send(response);
+      })
+      .catch(err => next(err));
+  });
+
+// Get headlines using user preferences - user login required
+newsRouter.route('/preferred')
+  .get(authenticate.verifyUser, (req, res, next) => {
+    User.findById(req.user.id)
+      .then(user => {
+        const options = {
+          page: 1
+        };
+        if (user.preferredSources.length) {
+          options['sources'] = user.preferredSources.join(',');
+        } else {
+          options['country'] = user.country;
+        }
+        newsapi.v2.topHeadlines(options)
+          .then(response => {
+            res.statusCode = 200;
+            res.setHeader('content-type', 'application/json');
+            res.send(response);
+          })
+          .catch(err => next(err));
+      })
+      .catch(err => next(err));
+  });
+
+// Perform query search with user preferences - user login required
+newsRouter.route('/preferred/search')
   .get(authenticate.verifyUser, (req, res, next) => {
     User.findById(req.user.id)
       .then(user => {
@@ -107,30 +127,9 @@ newsRouter.route('/search/:id')
           .then(response => {
             res.statusCode = 200;
             res.setHeader('content-type', 'application/json');
-            res.json(response);
+            res.send(response);
           })
           .catch(err => next(err));
-      })
-      .catch(err => next(err));
-  })
-
-newsRouter.route('/sources')
-  .get((req, res, next) => {
-    const options = {
-      category: 'general',
-      language: 'en',
-      country: 'us'
-    };
-    if (req.query) {
-      for (const key in req.query) {
-        options[key] = req.query[key];
-      }
-    }
-    newsapi.v2.sources(options)
-      .then(response => {
-        res.statusCode = 200;
-        res.setHeader('content-type', 'application/json');
-        res.json(response);
       })
       .catch(err => next(err));
   });
